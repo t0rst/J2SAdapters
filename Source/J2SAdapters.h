b4baed54bd006db775769f75201d27bb00728f8f
Torsten Louland <torsten.louland@satisfyingstructures.com>
2016-11-20 15:08:20 +0100

    Starter

---
+++ Source/J2SAdapters.h
@@ -0,0 +1,19 @@
+//
+//  J2SAdapters.h
+//  J2SAdapters
+//
+//  Created by Torsten Louland on 19/11/2016.
+//  Copyright © 2016 Satisfying Structures BVBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for J2SAdapters.
+FOUNDATION_EXPORT double J2SVersionNumber;
+
+//! Project version string for J2SAdapters.
+FOUNDATION_EXPORT const unsigned char J2SVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <J2SAdapters/PublicHeader.h>
+
+
